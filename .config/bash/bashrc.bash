6eb75635ca19866f8091e9bc15544954af4e0fe3
ay65535 <ay65535@icloud.com>
2023-05-18 13:06:00 +0900

    Add kind completion

--- .config/bash/bashrc.bash
+++ .config/bash/bashrc.bash
@@ -12,19 +12,22 @@ export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME/bash_completion"
 
 [[ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]] && . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
 
+[ ! -d "$XDG_CONFIG_HOME/bash-completion.d" ] && mkdir -p "$XDG_CONFIG_HOME/bash-completion.d"
+
 # kubectl
 if command -v kubectl >/dev/null; then
   if [[ ! -r "$XDG_CONFIG_HOME/bash-completion.d/kubectl_completion.bash" ]]; then
-    mkdir -p "$XDG_CONFIG_HOME/bash-completion.d"
     kubectl completion bash >"$XDG_CONFIG_HOME/bash-completion.d/kubectl_completion.bash"
   fi
   . "$XDG_CONFIG_HOME/bash-completion.d/kubectl_completion.bash"
 fi
 
-if [ -n "$http_proxy" ]; then
-  echo "proxy=$http_proxy" >"$HOME/curlrc"
-else
-  test -f "$HOME/curlrc" && rm "$HOME/curlrc"
+# kind
+if command -v kind >/dev/null; then
+  if [[ ! -r "$XDG_CONFIG_HOME/bash-completion.d/kind_completion.bash" ]]; then
+    kind completion bash >"$XDG_CONFIG_HOME/bash-completion.d/kind_completion.bash"
+  fi
+  . "$XDG_CONFIG_HOME/bash-completion.d/kind_completion.bash"
 fi
 
 # shellcheck source=/home/jetson/.config/fzf/fzf.bash
