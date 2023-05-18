706c6ea01a1305f54c08c83522a036b16503c316
ay65535 <ay65535@icloud.com>
2023-05-18 11:14:17 +0900

    Add kubectl completion

--- .config/bash/bashrc.bash
+++ .config/bash/bashrc.bash
@@ -3,9 +3,23 @@ if [ ! -d "$XDG_STATE_HOME"/bash ]; then
 fi
 export EDITOR=vi
 export HISTFILE="$XDG_STATE_HOME/bash/history"
+
+#
+# completions
+#
+
 export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME/bash_completion"
 
 [[ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]] && . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
+
+# kubectl
+if command -v kubectl >/dev/null; then
+  if [[ ! -r "$XDG_CONFIG_HOME/bash-completion.d/kubectl_completion.bash" ]]; then
+    mkdir -p "$XDG_CONFIG_HOME/bash-completion.d"
+    kubectl completion bash >"$XDG_CONFIG_HOME/bash-completion.d/kubectl_completion.bash"
+  fi
+  . "$XDG_CONFIG_HOME/bash-completion.d/kubectl_completion.bash"
+fi
 
 if [ -n "$http_proxy" ]; then
   echo "proxy=$http_proxy" >"$HOME/curlrc"
