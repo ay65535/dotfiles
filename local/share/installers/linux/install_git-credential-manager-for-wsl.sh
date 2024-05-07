fa62a4d81a07f4232d1921acf7661093b56427a6
ay65535 <ay65535@icloud.com>
2024-05-07 11:58:46 +0900

    Add script

---
+++ local/share/installers/linux/install_git-credential-manager-for-wsl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://learn.microsoft.com/ja-jp/windows/wsl/tutorials/wsl-git#git-credential-manager-setup
+
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
