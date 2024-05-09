84cafbfde1011f666bcbae8a6a76f4b7de50b54c
ay65535 <ay65535@icloud.com>
2024-05-09 16:57:07 +0900

    Update install script

--- local/share/installers/linux/install_homebrew.sh
+++ local/share/installers/linux/install_homebrew.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # https://docs.brew.sh/Installation.html#untar-anywhere-unsupported
 
 mkdir ~/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew
