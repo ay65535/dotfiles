c568425f957ec827f8b0929441d83cde989b9a40
ay65535 <ay65535@icloud.com>
2024-05-14 18:03:38 +0900

    install latest

--- local/share/installers/git/rbenv.sh
+++ local/share/installers/git/rbenv.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/env bash
 # https://github.com/rbenv/rbenv?tab=readme-ov-file#basic-git-checkout
 
+# rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-~/.rbenv/bin/rbenv init - bash
-# export PATH="/home/vscode/.rbenv/bin:${PATH}"
-# export PATH="/home/vscode/.rbenv/shims:${PATH}"
-# export RBENV_SHELL=bash
-# source '/home/vscode/.rbenv/completions/rbenv.bash'
-# command rbenv rehash 2>/dev/null
-# rbenv() {
-#   local command
-#   command="${1:-}"
-#   if [ "$#" -gt 0 ]; then
-#     shift
-#   fi
-#
-#   case "$command" in
-#   rehash|shell)
-#     eval "$(rbenv "sh-$command" "$@")";;
-#   *)
-#     command rbenv "$command" "$@";;
-#   esac
-# }
+# ~/.rbenv/bin/rbenv init - bash
+rbenv rehash
 
-rbenv install -l
+# ruby-build
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+latest_version=$(rbenv install -l | grep -E '^[1-9]\.' | tail -1)
+rbenv install "$latest_version"
+
+# upgrade
+# git -C "$(rbenv root)" pull --rebase
+# git -C "$(rbenv root)"/plugins/ruby-build pull --rebase
