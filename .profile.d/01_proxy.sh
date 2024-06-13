32a2c236cdf71fbeda40d48ab1b73fbaf335e890
ay65535 <ay65535@icloud.com>
2024-06-13 15:18:56 +0900

    Fix proxy setting

--- .profile.d/01_proxy.sh
+++ .profile.d/01_proxy.sh
@@ -9,19 +9,23 @@ if [ -f "$XDG_CACHE_HOME/proxy_server.sh" ]; then
 elif wsl || msys; then
   # shellcheck disable=SC2016
   WINIPINFO=$(powershell.exe -nop -c 'Get-NetConnectionProfile -IPv4Connectivity Internet | sv connectionProfile; $connectionProfile | Get-NetIPInterface -AddressFamily IPv4 | sort InterfaceMetric | select -First 1 | Get-NetIPAddress | select -exp IPAddress | Write-Host -NoNewLine; "," + $connectionProfile.NetworkCategory')
-  WIN_HTTP_PROXY=$(powershell.exe -nop -c 'gpv "HKCU:/Software/Microsoft/Windows/CurrentVersion/Internet Settings" AutoConfigURL' | tr -d '\r')
-  if [ -n "$WIN_HTTP_PROXY" ]; then
-    PAC_CONTENT=$(curl -fsSL --noproxy '*' "$WIN_HTTP_PROXY")
+  WIN_AUTO_CONFIG_URL=$(powershell.exe -nop -c 'gpv "HKCU:/Software/Microsoft/Windows/CurrentVersion/Internet Settings" AutoConfigURL' | tr -d '\r')
+  WIN_PROXY_SERVER=$(powershell.exe -nop -c 'gpv "HKCU:/Software/Microsoft/Windows/CurrentVersion/Internet Settings" ProxyServer' | tr -d '\r')
+  if [ -n "$WIN_PROXY_SERVER" ]; then
+    BYPASS_HOST=
+    PROXY_SERVER=$WIN_PROXY_SERVER
+  elif [ -n "$WIN_AUTO_CONFIG_URL" ]; then
+    PAC_CONTENT=$(curl -fsSL --noproxy '*' "$WIN_AUTO_CONFIG_URL")
     BYPASS_HOST=$(echo "$PAC_CONTENT" | grep -oE '\w+\.net')
     PROXY_SERVER=$(echo "$PAC_CONTENT" | grep -oP "(?<=PROXY )[\d.:]+" | grep -v '10.' | sort -u)
   fi
   if echo "$WINIPINFO" | grep -q Domain; then
     LOCATION_PROFILE=office
   else
-    case "$WIN_HTTP_PROXY" in
+    case "$WIN_AUTO_CONFIG_URL" in
     "" | *.jp*) LOCATION_PROFILE=home ;;
     *localhost*) LOCATION_PROFILE=vpn ;;
-    *) echo "error invalid value: $$WIN_HTTP_PROXY" ;;
+    *) echo "error invalid value: WIN_AUTO_CONFIG_URL='$WIN_AUTO_CONFIG_URL'" ;;
     esac
   fi
   echo -e "export PROXY_SERVER=$PROXY_SERVER\nexport LOCATION_PROFILE=$LOCATION_PROFILE${BYPASS_HOST:+\nBYPASS_HOST=$BYPASS_HOST}" >"$XDG_CACHE_HOME/proxy_server.sh"
