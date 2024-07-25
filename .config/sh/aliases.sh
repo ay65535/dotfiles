51fcf6c7e1e763dd8b44f9af5de06843c71f3780
ay65535 <ay65535@icloud.com>
2024-07-25 14:26:16 +0900

    reduce

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -13,24 +13,10 @@ alias dvl='docker volume list'
 alias dC=docker-compose
 
 if [ -n "$HOMEBREW_PREFIX" ]; then
-  #alias brew="$HOMEBREW_PREFIX/bin/brew"
-
-  alias c=cask
-  alias b=brew
-
-  alias cask='brew cask '
-  alias caski='cask info'
-  alias caskI='cask install'
   alias brews='brew search'
   alias brewu='brew update'
-  alias bupd='brew update'
   alias brewU='brew upgrade'
-  alias bupg='brew upgrade'
   alias brewl='brew list'
-  alias bls='brew list'
   alias brewi='brew info'
   alias brewI='brew install'
-  alias binst='brew install'
-  alias brewcl='brew cask list'
-  alias brewci='brew cask info'
 fi
