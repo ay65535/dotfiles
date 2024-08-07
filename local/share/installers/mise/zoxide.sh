2ef2f10e5654b5dd922aedebe7b1a225dffda47e
ay65535 <ay65535@icloud.com>
2024-08-07 23:18:44 +0900

    u

--- local/share/installers/mise/zoxide.sh
+++ local/share/installers/mise/zoxide.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
-mise plugins ls-remote | grep zoxide
+FLAVOR=${1:-mise}
 
-# zoxide/noble 0.9.3-1 amd64
-apt list zoxide
-
-# 0.9.4
-mise ls-remote zoxide
-
-# sudo apt -y install zoxide
-# apt list --installed zoxide
-
-# deps
-# sudo apt -y install
-mise install zoxide
-mise use --global zoxide
-
-# cargo install zoxide
+case "$FLAVOR" in
+apt)
+  apt list zoxide
+  # ==> zoxide/noble 0.9.3-1 amd64
+  sudo apt -y install zoxide
+  apt list --installed zoxide
+  ;;
+mise)
+  mise plugins ls-remote | grep zoxide
+  mise ls-remote zoxide
+  # ==> 0.9.4
+  mise install zoxide
+  mise use --global zoxide
+  ;;
+*brew)
+  brew info zoxide
+  # ==> zoxide: stable 0.9.4 (bottled)
+  brew install zoxide
+  ;;
+*)
+  echo 'usage: ./zoxide.sh (apt|mise|brew) # mise is default.' >&2
+  exit 1
+  ;;
+esac
 
 which -a zoxide
 zoxide --version
