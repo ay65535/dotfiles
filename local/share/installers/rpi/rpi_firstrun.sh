d5f615a1bb602612cd7aadcb7b1dd4d43530f588
ay65535 <ay65535@icloud.com>
2025-04-13 01:18:11 +0900

    Update rpi_firstrun.sh

--- local/share/installers/rpi/rpi_firstrun.sh
+++ local/share/installers/rpi/rpi_firstrun.sh
@@ -18,26 +18,23 @@ if [ ! -f /etc/apt/sources.list.d/raspi.list.orig ]; then
   sudo cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.orig
 fi
 
-# 以下のような警告を解消
-# Get:4 https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm InRelease [3,918 B]
-# Err:4 https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm InRelease
-#   The following signatures couldn't be verified because the public key is not available: NO_PUBKEY E77FC0EC34276B4B
-# Reading package lists... Done
-# W: GPG error: https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm InRelease: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY E77FC0EC34276B4B
-# E: The repository 'https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm InRelease' is not signed.
-# N: Updating from such a repository can't be done securely, and is therefore disabled by default.
-# N: See apt-secure(8) manpage for repository creation and user configuration details.
-
-apt-key list >apt-key.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E77FC0EC34276B4B
-
 # Warning: apt-key is deprecated. Manage keyring files in trusted.gpg.d instead (see apt-key(8)). という警告を修正
-sudo apt-key export E77FC0EC34276B4B | sudo gpg --dearmor -o /usr/share/keyrings/raspbian-archive-keyring.gpg
+sudo mkdir -m 0755 -p /etc/apt/keyrings/
 
-echo 'deb [signed-by=/usr/share/keyrings/raspbian-archive-keyring.gpg] https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm main' | sudo tee /etc/apt/sources.list.d/raspi.list
+# 鍵ID: E77FC0EC34276B4B のRaspbian公開鍵を取得
+curl -fsSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xE77FC0EC34276B4B' |
+  gpg --dearmor |
+    sudo tee /etc/apt/keyrings/raspbian-archive-keyring.gpg >/dev/null
+
+# リポジトリの設定を追加し、取得した鍵を使用
+echo 'deb [signed-by=/etc/apt/keyrings/raspbian-archive-keyring.gpg] https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm main' |
+  sudo tee /etc/apt/sources.list.d/raspi.list
+
 cat /etc/apt/sources.list.d/raspi.list
 
 # N: Skipping acquire of configured file 'main/binary-armhf/Packages' as repository 'https://ftp.udx.icscoe.jp/Linux/raspbian/multiarch bookworm InRelease' doesn't support architecture 'armhf' という警告を修正
+dpkg --print-architecture
+dpkg --print-foreign-architectures
 sudo dpkg --remove-architecture armhf
 
 #
