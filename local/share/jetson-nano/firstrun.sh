9be0a41beddcc366a8145ce01deb1f48d193c7c2
ay65535 <ay65535@icloud.com>
2025-05-11 23:58:14 +0900

    Jetson Nanoのfirstrun.shを更新: NVIDIA Tegraの設定ファイルの移動とシステム設定の確認を追加

--- local/share/jetson-nano/firstrun.sh
+++ local/share/jetson-nano/firstrun.sh
@@ -8,3 +8,40 @@ cat /proc/version
 
 uname -a
 # ==> Linux jetson-nano 4.9.337-tegra #1 SMP PREEMPT Mon Nov 4 23:41:41 PST 2024 aarch64 aarch64 aarch64 GNU/Linux
+
+# (existing)
+ls -l /etc/ld.so.conf.d/nvidia-tegra*
+cat /etc/ld.so.conf.d/nvidia-tegra.conf.dpkg-old
+cat /etc/ld.so.conf.d/nvidia-tegra.conf
+# --- /etc/ld.so.conf.d/nvidia-tegra.conf 2025-05-08 00:19:18.893852342 +0900
+# +++ /etc/ld.so.conf.d/nvidia-tegra.conf.dpkg-new        2024-11-05 16:45:15.000000000 +0900
+# @@ -1 +1,11 @@
+# -/usr/lib/aarch64-linux-gnu/tegra
+# +/usr/lib/tegra
+ls -l /usr/lib/aarch64-linux-gnu/tegra
+# ==> OK
+ls -l /usr/lib/tegra
+# ==> ls: cannot access '/usr/lib/tegra': No such file or directory
+sudo mv /etc/ld.so.conf.d/nvidia-tegra.conf{,.dpkg-new}
+sudo mv /etc/ld.so.conf.d/nvidia-tegra.conf{.dpkg-old,}
+ls -l /etc/ld.so.conf.d/nvidia-tegra*
+diff -u /etc/ld.so.conf.d/nvidia-tegra.conf{,.dpkg-new}
+
+# (new file)
+# ls -l /etc/systemd/nv-oem-config-post*
+# cat /etc/systemd/nv-oem-config-post.sh
+
+# (new file)
+# ls -l /etc/systemd/nv-oem-config*
+# cat /etc/systemd/nv-oem-config.sh
+
+# https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+cat /proc/sys/fs/inotify/max_user_watches
+# ==> 8192
+
+[ ! -f /etc/sysctl.conf.orig ] && sudo cp -a /etc/sysctl.conf /etc/sysctl.conf.orig
+sudo vim /etc/sysctl.conf
+# fs.inotify.max_user_watches=524288
+
+# The new value can then be loaded in by running
+sudo sysctl -p
