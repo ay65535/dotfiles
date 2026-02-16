9e4daf154826bb6e7d173805e2bfdf141c92fbfc
ay65535 <ay65535@icloud.com>
2026-02-16 20:47:54 +0900

    config.tomlのツール設定を更新し、dotnetのバージョンを変更。install_mise-apt.shとruby.shのインストール手順を整理

--- local/share/installers/mise/install_mise-apt.sh
+++ local/share/installers/mise/install_mise-apt.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # https://mise.jdx.dev/installing-mise.html#apt
 
-sudo apt update -y && sudo apt install -y gpg sudo wget curl
-sudo install -dm 755 /etc/apt/keyrings
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
-ARCH=$(dpkg --print-architecture) # アーキテクチャを取得
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=${ARCH}] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-sudo apt update
-apt list mise
+# On Ubuntu 26.04+, mise is available via a PPA:
+sudo add-apt-repository -y ppa:jdxcode/mise
+sudo apt update -y
 sudo apt install -y mise
+
+# sudo apt update -y && sudo apt install -y gpg sudo wget curl
+# sudo install -dm 755 /etc/apt/keyrings
+# wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+# ARCH=$(dpkg --print-architecture) # アーキテクチャを取得
+# echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=${ARCH}] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+# sudo apt update
+# apt list mise
+# sudo apt install -y mise
 
 # which -a mise
 # mise --version
--- local/share/installers/mise/ruby.sh
+++ local/share/installers/mise/ruby.sh
@@ -4,10 +4,13 @@ mise plugins ls-remote | grep ruby
 mise plugins ls --core | grep ruby
 
 mise ls-remote ruby 3
-sudo apt -y install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
-mise install ruby 3
-mise use --global ruby@latest
+# https://github.com/rbenv/ruby-build/wiki#ubuntudebianmint
+sudo apt -y install build-essential autoconf libssl-dev libyaml-dev zlib1g-dev libffi-dev libgmp-dev rustc
+# sudo apt -y install patch libreadline6-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev
+# sudo apt -y uuid-dev
+mise use -g ruby@3
 mise ls
+mise ls -g
 
 which -a ruby
 ruby -v
