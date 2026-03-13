da9a8b839d2b89cc01ed80839dff0b32e3627b33
ay65535 <ay65535@icloud.com>
2026-03-14 03:52:19 +0900

    fix: add daemon proxy setting

--- local/share/installers/ubuntu/firstrun/01_configure_docker_proxy.sh
+++ local/share/installers/ubuntu/firstrun/01_configure_docker_proxy.sh
@@ -30,6 +30,7 @@ else
         "default": {
             "httpProxy": "${http_proxy:?}",
             "httpsProxy": "${https_proxy:?}"
+            "noProxy": "${no_proxy:?}"
         }
     }
 }
@@ -43,3 +44,31 @@ cat "$TARGET"
 # restore
 # sudo rm ${TARGET}
 # sudo rm ${TARGET}.orig
+
+#
+# Daemon proxy
+#
+
+# https://docs.docker.com/engine/daemon/proxy/
+
+# check
+sudo systemctl show --property=Environment docker
+
+# MEMO:
+#   /etc/docker/daemon.json はdockerの設定ファイル
+#   /etc/systemd/system/docker.service.d/http-proxy.conf はsystemdの設定ファイル
+#   両方指定した場合はdaemon.jsonが優先される
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "proxies": {
+    "http-proxy": "${http_proxy:?}",
+    "https-proxy": "${https_proxy:?}",
+    "no-proxy": "${no_proxy:?}"
+  }
+}
+EOF
+
+# daemon再起動
+sudo systemctl restart docker
